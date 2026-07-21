65f365a1b9efcb6bff06efd247a052c1f94e56a3
Nguyễn Thanh Nhựt - PS50048 <93459542+nguyenthanhnhut2004@users.noreply.github.com>
2026-07-21 22:12:00 +0700

    asm tien do chua hoan thien

---
+++ LAP3/asm_menucode .C
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    
+    {
+        int chon;
+        printf("|======================================|\n");
+        printf("|        MENU CHUONG TRINH ASM 1       |\n");
+        printf("|======================================|\n");
+        printf("| 1:KIEM TRA SO NGUYEN                 |\n");
+        printf("| 2:TIM UOC CHUNG VA BOI SO CUA 2 SO   |\n");
+        printf("| 3:CHUONG TRINH TINH TIEN QUAN KARAOKE|\n");
+        printf("| 4:TINH TIEN DIEN                     |\n");
+        printf("| 5:TINH LAI XUAT VAY NGAN HANG TRA GOP|\n");
+        printf("| 6:SAP XEP THONG TIN SINH VIEN        |\n");
+        printf("| 7:CHUONG TRINH TINH TOAN PHAN SO     |\n");
+        printf("| 8:THOAT CHUONG TRINH                 |\n");
+        printf("|======================================|\n");
+        printf("Vui long chon chuc nang tu 1 den 8\n");
+        switch (chon);
+        {
+        case 1;
+            printf("/nBan da chon chuc nang 1\n");
+            printf(" Kiem Tra so Nguyen\n")
+
+            break;
+        
+        default:
+            return 0;
+        }
+    }
+}
