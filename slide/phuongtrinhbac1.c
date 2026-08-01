470d13769c9a7a436a0181496dba1f960fe58179
nhutnguyenps50048 <thanhnhut01052004@gmail.com>
2026-08-01 10:47:54 +0700

    SLIDE 6

--- slide/phuongtrinhbac1.c
+++ slide/phuongtrinhbac1.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 
 int main() {
-    int a, b;
-    float x;
+    float a, b, x;
 
-    printf("Nhap he so a:\n ");
-    if (scanf("%d", &a) ) 
-        
-    printf("Nhap he so b:\n ");
-    if (scanf("%d", &b) );
+    printf("Nhap he so a: ");
+    scanf("%f", &a);
+
+    printf("Nhap he so b: ");
+    scanf("%f", &b);
 
     if (a == 0) {
-        printf("Phuong trinh vo nghiem.\n");
+        if (b == 0) {
+            printf("Phuong trinh co vo so nghiem.\n");
+        } else {
+            printf("Phuong trinh vo nghiem.\n");
+        }
     } else {
-        x = -(float)b / a;
+        x = -b / a;
         printf("Nghiem cua phuong trinh: x = %.2f\n", x);
     }
 
     return 0;
 }
+
  
---
+++ slide6/demo.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int B[5] = {3, 6, 7, 2, 4};
+    printf("%d\n", B[0]);
+    for (int i = 0; i < 5; i++) {
+        printf("B[%d] = %d\n", i, B[i]);
+    }
+
+    int A[3][6] = {
+        {1, 2, 3, 4, 5, 6},
+        {7, 8, 9, 10, 11, 12},
+        {13, 14, 15, 16, 17, 18}
+    };
+
+    printf("\nMa tran A[3][6]:\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 6; j++) {
+            printf("A[%d,%d]=%2d\t", i, j, A[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
